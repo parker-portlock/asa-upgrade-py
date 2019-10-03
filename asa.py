7a5334e7a56ae913fab0078e6f803957d8607de2
Parker Portlock <parker.portlock@alertlogic.com>
2019-10-03 10:34:10 -0500

    add error handling

--- asa.py
+++ asa.py
@@ -4,6 +4,9 @@ import netmiko
 
 def failover (host,username,password):
     from netmiko import ConnectHandler
-    net_connect = ConnectHandler(device_type='cisco_asa',ip=host,username=username,password=password)
-    failAct = "failover exec standby failover active"
-    net_connect.send_command(failAct) 
+    try:
+        net_connect = ConnectHandler(device_type='cisco_asa',ip=host,username=username,password=password)
+        failAct = "failover exec standby failover active"
+        net_connect.send_command(failAct) 
+    except:
+        pass
