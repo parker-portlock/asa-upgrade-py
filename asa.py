6ed26a2578e6ef319a961032efbf927bcf362fff
Parker Portlock <parker.portlock@alertlogic.com>
2019-10-03 09:58:10 -0500

    import ConnectHandler from netmiko

--- asa.py
+++ asa.py
@@ -3,6 +3,7 @@
 import netmiko
 
 def failover (host,username,password):
+    from netmiko import ConnectHandler
     net_connect = ConnectHandler(device_type='cisco_asa',ip=host,username=username,password=password)
     failAct = "failover exec standby failover active"
     net_connect.send_command(failAct) 
